7233e6bd0b3659331f806143462f3e890ed262cc
frostspb <acyb@mail.ru>
2021-07-12 17:22:08 +0300

    fix hgf

--- hourglass/campaigns/api/serializers.py
+++ hourglass/campaigns/api/serializers.py
@@ -64,60 +64,7 @@ class TacticsSerializer(serializers.ModelSerializer):
         )
 
 
-class HourglassSerializer(serializers.ModelSerializer):
-    end_date = serializers.SerializerMethodField()
-    TA = serializers.SerializerMethodField()
-    duration = serializers.SerializerMethodField()
-    state = serializers.SerializerMethodField()
-    velocity = serializers.SerializerMethodField()
-    total_goal = serializers.SerializerMethodField()
-    generated = serializers.SerializerMethodField()
-    generated_pos = serializers.SerializerMethodField()
-    sections = SectionsSettingsSerializer(read_only=True, many=True)
-    #tactics = TacticsSerializer(read_only=True, many=True)
-    tactics = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Campaign
-        fields = (
-            "end_date", "TA", "duration", "state", "velocity", "pacing", "integration",
-            "total_goal", "generated", "generated_pos", "sections", "tactics",
-        )
-
-    def get_end_date(self, instance):
-        if instance.kind == instance.CampaignKinds.STANDARD:
-            return instance.initial_end_date.strftime(settings.ENDPOINT_DATE_FORMAT)
-
-        if instance.end_date:
-            return instance.end_date.strftime(settings.ENDPOINT_DATE_FORMAT)
-
-    def get_TA(self, instance):
-        return instance.ta
-
-    def get_duration(self, instance):
-        return instance.duration
-
-    def get_state(self, instance):
-        return instance.state
-
-    def get_velocity(self, instance):
-        return instance.velocity
-
-    def get_total_goal(self, instance):
-        return instance.total_goal
-
-    def get_generated(self, instance):
-        return instance.generated
-
-    def get_generated_pos(self, instance):
-        return instance.generated_pos
-
-    def get_tactics(self, instance):
-        model_tactics = instance.tactics.values_list('id', flat=True)
-        t = Tactics.objects.all().values()
-        for i in t:
-            i['active'] = True if i.get('id') in model_tactics else False
-        return t
 
 
 class CampaignSettingsSerializer(serializers.ModelSerializer):
@@ -386,3 +333,77 @@ class CampaignSerializer(serializers.ModelSerializer):
                 res = instance.end_date
         if res:
             return res.strftime(settings.ENDPOINT_DATE_FORMAT)
+
+
+class HourglassSerializer(serializers.ModelSerializer):
+    end_date = serializers.SerializerMethodField()
+    TA = serializers.SerializerMethodField()
+    duration = serializers.SerializerMethodField()
+    state = serializers.SerializerMethodField()
+    velocity = serializers.SerializerMethodField()
+    total_goal = serializers.SerializerMethodField()
+    generated = serializers.SerializerMethodField()
+    generated_pos = serializers.SerializerMethodField()
+    sections = SectionsSettingsSerializer(read_only=True, many=True)
+    #tactics = TacticsSerializer(read_only=True, many=True)
+    tactics = serializers.SerializerMethodField()
+
+    assets = AssetsSectionSerializer(many=True, read_only=True)
+    intents = IntentFeedsSectionSerializer(many=True, read_only=True)
+    titles = JobTitlesSectionSerializer(many=True, read_only=True)
+    industries = IndustriesSectionSerializer(many=True, read_only=True)
+    revenues = RevenueSectionSerializer(many=True, read_only=True)
+    companies_size = CompanySizeSectionSerializer(source="companies", many=True, read_only=True)
+    geolocations = GeolocationsSectionSerializer(many=True, read_only=True)
+    bants = BANTQuestionsSectionSerializer(many=True, read_only=True)
+    custom_questions = CustomQuestionsSectionSerializer(source="cqs", many=True, read_only=True)
+    abms = ABMSectionSerializer(many=True, read_only=True)
+    install_base = InstallBaseSectionSerializer(source="ibs", many=True, read_only=True)
+    fair_trades = FairTradeSectionSerializer(many=True, read_only=True)
+    lead_cascades = LeadCascadeProgramSectionSerializer(many=True, read_only=True)
+    nurturings = NurturingSectionSerializer(many=True, read_only=True)
+    creatives = CreativesSectionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Campaign
+        fields = (
+            "end_date", "TA", "duration", "state", "velocity", "pacing", "integration",
+            "total_goal", "generated", "generated_pos", "sections", "tactics", "dashboard_string_count", "assets", "intents", "titles",
+            "industries", "revenues", "companies_size", "geolocations", "bants", "custom_questions", "abms",
+            "install_base", "fair_trades", "lead_cascades", "nurturings", "creatives"
+        )
+
+    def get_end_date(self, instance):
+        if instance.kind == instance.CampaignKinds.STANDARD:
+            return instance.initial_end_date.strftime(settings.ENDPOINT_DATE_FORMAT)
+
+        if instance.end_date:
+            return instance.end_date.strftime(settings.ENDPOINT_DATE_FORMAT)
+
+    def get_TA(self, instance):
+        return instance.ta
+
+    def get_duration(self, instance):
+        return instance.duration
+
+    def get_state(self, instance):
+        return instance.state
+
+    def get_velocity(self, instance):
+        return instance.velocity
+
+    def get_total_goal(self, instance):
+        return instance.total_goal
+
+    def get_generated(self, instance):
+        return instance.generated
+
+    def get_generated_pos(self, instance):
+        return instance.generated_pos
+
+    def get_tactics(self, instance):
+        model_tactics = instance.tactics.values_list('id', flat=True)
+        t = Tactics.objects.all().values()
+        for i in t:
+            i['active'] = True if i.get('id') in model_tactics else False
+        return t
