5c2336266dfd7766bd0acaf46124ee86f798c509
frostspb <acyb@mail.ru>
2021-10-12 12:10:32 +0300

    1

--- hourglass/references/api/serializers.py
+++ hourglass/references/api/serializers.py
@@ -25,7 +25,7 @@ class GeolocationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Geolocations
         fields = (
-            'id', 'name',
+            'id', 'name', 'code'
         )
 
 
