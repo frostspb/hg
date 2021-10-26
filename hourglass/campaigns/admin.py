27135d0fb45328e1145cc637cc56b6e84b68f625
frostspb <acyb@mail.ru>
2021-10-26 19:21:18 +0300

    1

--- hourglass/campaigns/admin.py
+++ hourglass/campaigns/admin.py
@@ -253,7 +253,7 @@ class TeamsAdmin(admin.TabularInline):
     extra = 0
 
     fields = ['name', 'team_lead', 'team_member1', 'team_member2', 'team_member3', 'team_member4',
-              'rejected_percent', 'leads_rejected', 'rejected_delivered']
+              'rejected_percent', 'leads_rejected', 'percent_delivered']
     readonly_fields = ['leads_rejected']
     insert_after = 'generated_leads'
 
