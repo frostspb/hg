3f93d87ee82b22e759929b3397ccc036e4519283
frostspb <acyb@mail.ru>
2022-02-10 12:02:25 +0300

    1

--- hourglass/users/admin.py
+++ hourglass/users/admin.py
@@ -2,11 +2,27 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-from .models import AuditEntry
+from .models import AuditUserEntry
 
 
 from .forms import UserChangeForm, UserCreationForm
 User = get_user_model()
+
+
+class AuditEntrySectionAdmin(admin.TabularInline):
+    model = AuditUserEntry
+    extra = 0
+    classes = ['collapse']
+    readonly_fields = ['action', 'ip', 'created']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(User)
@@ -24,9 +40,6 @@ class UserAdmin(auth_admin.UserAdmin):
     list_display = ["username", "is_staff", "is_active", "date_joined", "last_login", "email"]
     search_fields = ["username", "email", "id", ]
     ordering = ('-date_joined',)
+    inlines = [AuditEntrySectionAdmin]
 
 
-@admin.register(AuditEntry)
-class AuditEntryAdmin(admin.ModelAdmin):
-    list_display = ['action', 'username', 'ip', 'created']
-    list_filter = ['action',]
--- hourglass/users/models.py
+++ hourglass/users/models.py
@@ -32,31 +32,26 @@ class User(AbstractUser):
             return self.photo.url
 
 
-class AuditEntry(TimeStampedModel):
+class AuditUserEntry(TimeStampedModel):
 
     action = models.CharField(max_length=64)
     ip = models.GenericIPAddressField(null=True)
-    username =  models.CharField(max_length=256, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __unicode__(self):
-        return '{0} - {1} - {2}'.format(self.action, self.username, self.ip)
+        return '{0} - {1} - {2}'.format(self.action, self.user.username, self.ip)
 
 
 @receiver(user_logged_in)
 def user_logged_in_callback(sender, request, user, **kwargs):
 
     ip = request.META.get('REMOTE_ADDR')
-    AuditEntry.objects.create(action='user_logged_in', ip=ip, username=user.username)
+    AuditUserEntry.objects.create(action='user_logged_in', ip=ip, user=user)
 
 
 @receiver(user_logged_out)
 def user_logged_out_callback(sender, request, user, **kwargs):
 
     ip = request.META.get('REMOTE_ADDR')
-    AuditEntry.objects.create(action='user_logged_out', ip=ip, username=user.username)
-
-
-@receiver(user_login_failed)
-def user_login_failed_callback(sender, credentials, **kwargs):
-
-    AuditEntry.objects.create(action='user_login_failed', username=credentials.get('username', None))
+    AuditUserEntry.objects.create(action='user_logged_out', ip=ip, username=user)
